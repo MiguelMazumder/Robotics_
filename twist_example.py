9916154f65d015006bceb59b301d6de763dd9212
MiguelMazumder <72771218+MiguelMazumder@users.noreply.github.com>
2023-03-21 15:42:39 -0400

    Update twist_example.py

--- twist_example.py
+++ twist_example.py
@@ -1,17 +1,15 @@
 """ Example of how to set attributes in ROS messages """
 from geometry_msgs.msg import Twist
 
-
 def twist_fill():
     '''
     Fill a twist message with non-zero values
     '''
     msg = Twist()
-    msg.linear.x = 2
-    msg.linear.y = 3 
-    msg.linear.z= 5 
-    msg.angular.x = 7 
-    msg.angular.y = 6
-    msg.angular.z = 9
+    msg.linear.x=2.0
+    msg.linear.y=3.0
+    msg.linear.z=5.0
+    msg.angular.x=7.0
+    msg.angular.y=6.0
+    msg.angular.z=9.0
     return msg
-
