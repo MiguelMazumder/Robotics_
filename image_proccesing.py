8d947779b592fdf1306b4ecb6013e67ea4001ea2
MiguelMazumder <72771218+MiguelMazumder@users.noreply.github.com>
2023-04-26 00:56:44 -0400

    Update image_proccesing.py

--- image_proccesing.py
+++ image_proccesing.py
@@ -16,8 +16,8 @@ def classifier_parameters():
     """
     # This implementation is a stub. Modify with your thresholds.
 
-    bound_low = (0, 0, 0)
-    bound_high = (0, 0, 0)
+    bound_low = (25, 44, 150)
+    bound_high = (90, 134, 193)
     return bound_low, bound_high
 
 
@@ -76,8 +76,17 @@ def pixel_count(img):
     and zero (negative label)
     """
     # This implementation is a stub. Substitute with your own code.
-
-    nb_positive = np.count_nonzero(img)
+    height, width = img.shape
+    # Initialize a counter for non-zero pixels
+    count = 0
+    # Loop through each pixel in the image
+    for y in range(height):
+        for x in range(width):
+            # Check if the pixel value is non-zero
+            if img[y, x] != 0:
+                # Increment the counter
+                count += 1
+    nb_positive=count
     nb_negative = img.size - nb_positive
     return nb_positive, nb_negative
 
@@ -88,8 +97,8 @@ def precision_recall(true_positive, false_positive, false_negative):
     """
     # This implementation is a stub. Substitute with your own code.
 
-    precision = 0
-    recall = 0
+    precision = float(true_positive/(true_positive+false_positive))
+    recall = float(true_positive/(true_positive+false_negative))
     return precision, recall
 
 
@@ -98,10 +107,35 @@ def segmentation_statistics(filename_positive, filename_negative):
     Print segmentation statistics for two files with known segmentation
     """
     # This implementation is a stub. Substitute with your own code.
-
-    precision = 0
-    recall = 0
-    return precision, recall
+    high_,low_=classifier_parameters()
+    img_positive = cv2.imread(filename_positive)
+    img_negative = cv2.imread(filename_negative)
+    img_positive_hsv=cv2.cvtColor(img_positive,cv2.COLOR_BGR2HSV)
+    img_negative_hsv=cv2.cvtColor(img_negative,cv2.COLOR_BGR2HSV)
+    #find if it fits in range 0 if it doesnt, 255 if it does
+    img_pos_yhat=cv2.inRange(img_positive_hsv,low_,high_)
+    img_neg_yhat=cv2.inRange(img_negative_hsv, low_, high_)
+    p1,f1=pixel_count(img_pos_yhat)
+    p2,f2=pixel_count(img_neg_yhat)
+    precision,recall=precision_recall(p1, f2, f1)
+    # Display statistics
+    #number of points in image
+    print(f"Total Number of points in {filename_positive}: {p1+f1}")
+    print(f"Total Number of points in {filename_negative}: {p2+f2}")
+    #number of positives and negatives in each image
+    print(f"Total number of positives in {filename_positive}: {p1}")
+    print(f"Total number of negatives in {filename_positive}: {f1}")
+    print(f"Total number of positives in {filename_negative}: {p2}")
+    print(f"Total number of negatives in {filename_negative}: {f2}")
+    #number of false positives and false negatives
+    print(f"Number of false positives in {filename_positive}: {0}")
+    print(f"Number of false negatives in {filename_positive}: {f1}")
+    print(f"Number of false positives in {filename_negative}: {p2}")
+    print(f"Number of false negatives in {filename_negative}: {0}")
+    #print recall
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    return precision,recall
 
 
 def image_centroid_test():
@@ -121,7 +155,20 @@ def image_centroid_test():
     cv2.waitKey()
     cv2.imshow('test_segmented', line)
     cv2.waitKey()
-
+def image_statistics_test():
+    #do it with train and cross
+    precision_train,recall_train=segmentation_statistics('train_positive.png', 'train_negative.png')
+    precision_cross,recall_cross=segmentation_statistics('train_positive.png', 'train_negative.png')
+    #apply same thresholds to line-training and line-cross
+    line_train=cv2.imread('line-training.png')
+    train_hsv=cv2.cvtColor(line_train,cv2.COLOR_BGR2HSV)
+    line_cross=cv2.imread('line-cross.png')
+    cross_hsv=cv2.cvtColor(line_cross,cv2.COLOR_BGR2HSV)
+    high_,low_=classifier_parameters()
+    train_mask=cv2.inRange(train_hsv,low_,high_)
+    cross_mask=cv2.inRange(cross_hsv,low_,high_)
+    cv2.imwrite('line-training_mask',train_mask)
+    cv2.imwrite('cross-training_mask',cross_mask)
 
 if __name__ == '__main__':
     test()
